375e12603fc20cb2da466a25ae328fd61aaf9012
Vignesh Sadhu <vigneshsadhu143@gmail.com>
2024-01-23 21:13:04 +0530

    backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from "./user"
+import pin from "./pin"
+import postedBy from "./postedBy"
+import save from "./save"
+import comment from "./comment"
+export const schemaTypes = [user,pin,postedBy,save,comment]
